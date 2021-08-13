aed2a66ef0a5eb435438d4be5a6dba49e0689459
TianyiLi <e0991100238@gmail.com>
2021-08-13 12:26:55 +0800

    fix types

--- index.d.ts
+++ index.d.ts
@@ -1,13 +1,11 @@
 import { TailwindConfig } from 'tailwindcss/tailwind-config';
 
-declare module 'tailwind-purge' {
-  type params = {
-    tailwindConfig?: TailwindConfig;
-    baseCss: string;
-    baseHTML: string;
-  };
+type params = {
+  tailwindConfig?: TailwindConfig;
+  baseCss: string;
+  baseHTML: string;
+};
 
-  type main = (config: params) => Promise<string>;
+type main = (config: params) => Promise<string>;
 
-  export = main;
-}
+export = main;
